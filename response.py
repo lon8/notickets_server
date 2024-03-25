dd00e1822ccbb4b0d93ae80fc2eb6231954001ac
lon8 <vlad.kanatik@ya.ru>
2024-03-25 14:54:25 +0300

    more upate

--- response.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-# req = requests.get('http://localhost:8000/event_info?event_id=676959&user_token=1702901399752-134904')
-req = requests.get('http://localhost:8000/get_events/')
-
-print(req.text)
